ad4afb7263d55ca4bfe32f3e963aa475d01fb1b7
Saminda Peramuna <saminda_lk@msn.com>
2017-09-15 20:40:50 +0530

    Now can upload image.

--- WebOCR/src/main/resources/static/js/index.js
+++ WebOCR/src/main/resources/static/js/index.js
@@ -1,84 +1,113 @@
 /**
  * Contains the functionality of the index.html
  */
-$(document).ready(
-		function() {
-			$('img#example').selectAreas({
-				minSize : [ 10, 10 ],
-				onChanged : debugQtyAreas,
-				width : 500,
-				areas : [ {
-					x : 10,
-					y : 20,
-					width : 60,
-					height : 100,
-				} ]
-			});
-			$('#btnView').click(function() {
-				var areas = $('img#example').selectAreas('areas');
-				displayAreas(areas);
-			});
-			$('#btnViewRel').click(function() {
-				var areas = $('img#example').selectAreas('relativeAreas');
-				displayAreas(areas);
-			});
-			$('#btnReset').click(function() {
-				output("reset")
-				$('img#example').selectAreas('reset');
-			});
-			$('#btnDestroy').click(function() {
-				$('img#example').selectAreas('destroy');
+$(document).ready(function() {
+	$('img#originalImage').selectAreas({
+		onChanged : debugQtyAreas
+	});
 
-				output("destroyed")
-				$('.actionOn').attr("disabled", "disabled");
-				$('.actionOff').removeAttr("disabled")
-			});
-			$('#btnCreate').attr("disabled", "disabled").click(function() {
-				$('img#example').selectAreas({
-					minSize : [ 10, 10 ],
-					onChanged : debugQtyAreas,
-					width : 500,
-				});
+	$('#btnView').click(function() {
+		var areas = $('img#originalImage').selectAreas('areas');
+		displayAreas(areas);
+	});
 
-				output("created")
-				$('.actionOff').attr("disabled", "disabled");
-				$('.actionOn').removeAttr("disabled")
-			});
-			$('#btnNew').click(function() {
-				var areaOptions = {
-					x : Math.floor((Math.random() * 200)),
-					y : Math.floor((Math.random() * 200)),
-					width : Math.floor((Math.random() * 100)) + 50,
-					height : Math.floor((Math.random() * 100)) + 20,
-				};
-				output("Add a new area: " + areaToString(areaOptions))
-				$('img#example').selectAreas('add', areaOptions);
-			});
-			$('#btnNews').click(
-					function() {
-						var areaOption1 = {
-							x : Math.floor((Math.random() * 200)),
-							y : Math.floor((Math.random() * 200)),
-							width : Math.floor((Math.random() * 100)) + 50,
-							height : Math.floor((Math.random() * 100)) + 20,
-						}, areaOption2 = {
-							x : areaOption1.x + areaOption1.width + 10,
-							y : areaOption1.y + areaOption1.height - 20,
-							width : 50,
-							height : 20,
-						};
-						output("Add a new area: " + areaToString(areaOption1)
-								+ " and " + areaToString(areaOption2))
-						$('img#example').selectAreas('add',
-								[ areaOption1, areaOption2 ]);
-					});
+	$('#btnViewRel').click(function() {
+		var areas = $('img#originalImage').selectAreas('relativeAreas');
+		displayAreas(areas);
+	});
+
+	$('#btnReset').click(function() {
+		output("reset");
+		$('img#originalImage').selectAreas('reset');
+	});
+
+	$('#btnDestroy').click(function() {
+		$('img#originalImage').selectAreas('destroy');
+
+		output("destroyed");
+		$('.actionOn').attr("disabled", "disabled");
+		$('.actionOff').removeAttr("disabled");
+	});
+
+	$('#btnCreate').attr("disabled", "disabled").click(function() {
+		$('img#originalImage').selectAreas({
+			onChanged : debugQtyAreas
 		});
+
+		output("created");
+		$('.actionOff').attr("disabled", "disabled");
+		$('.actionOn').removeAttr("disabled");
+	});
+
+	$('#btnNew').click(function() {
+		var areaOptions = {
+		x : Math.floor((Math.random() * 200)),
+		y : Math.floor((Math.random() * 200)),
+		width : Math.floor((Math.random() * 100)) + 50,
+		height : Math.floor((Math.random() * 100)) + 20,
+		};
+
+		output("Add a new area: " + areaToString(areaOptions));
+		$('img#originalImage').selectAreas('add', areaOptions);
+	});
+
+	$('#btnNews').click(function() {
+		var areaOption1 = {
+		x : Math.floor((Math.random() * 200)),
+		y : Math.floor((Math.random() * 200)),
+		width : Math.floor((Math.random() * 100)) + 50,
+		height : Math.floor((Math.random() * 100)) + 20,
+		}, areaOption2 = {
+		x : areaOption1.x + areaOption1.width + 10,
+		y : areaOption1.y + areaOption1.height - 20,
+		width : 50,
+		height : 20,
+		};
+
+		output("Add a new area: " + areaToString(areaOption1) + " and " + areaToString(areaOption2));
+
+		$('img#originalImage').selectAreas('add', [ areaOption1, areaOption2 ]);
+	});
+
+	// On file input changed.
+	$('#imageInput').on('change', function(event) {
+		var imgElem = document.getElementById("originalImage");
+		var tgt = event.target || window.event.srcElement;
+		var files = tgt.files;
+
+		// Check for FileReader support.
+		if (FileReader && files && files.length) {
+			var fr = new FileReader();
+
+			fr.onload = function() {
+				imgElem.src = fr.result;
+			}
+
+			fr.readAsDataURL(files[0]);
+		}
+
+	});
+
+	// On image has completed loading.
+	$('#originalImage').on('load', function(event) {
+		var imgElem = document.getElementById("originalImage");
+
+		// Resize container to fit image.
+		$('.image-decorator').css('width', imgElem.width);
+		$('.image-decorator').css('height', imgElem.height);
+
+		// Destroy and recreate the plug-in on the new image.
+		$('img#originalImage').selectAreas('destroy');
+		$('img#originalImage').selectAreas({
+			onChanged : debugQtyAreas
+		});
+	});
+});
 
 var selectionExists;
 
 function areaToString(area) {
-	return (typeof area.id === "undefined" ? "" : (area.id + ": ")) + area.x
-			+ ':' + area.y + ' ' + area.width + 'x' + area.height + '<br />'
+	return (typeof area.id === "undefined" ? "" : (area.id + ": ")) + area.x + ':' + area.y + ' ' + area.width + 'x' + area.height + '<br />'
 }
 
 function output(text) {
@@ -88,13 +117,13 @@ function output(text) {
 // Log the quantity of selections
 function debugQtyAreas(event, id, areas) {
 	console.log(areas.length + " areas", arguments);
-};
+}
 
-// Display areas coordinates in a div
+// Display areas coordinates in a DIV
 function displayAreas(areas) {
 	var text = "";
 	$.each(areas, function(id, area) {
 		text += areaToString(area);
 	});
 	output(text);
-};
+}
