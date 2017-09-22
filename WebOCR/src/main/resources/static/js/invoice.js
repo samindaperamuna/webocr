b681b6c8c3edf4da981abe1ae0e3a0b7d47b2557
Saminda Peramuna <saminda_lk@msn.com>
2017-09-22 15:07:50 +0530

    Finalised loading items from the OCR.

--- WebOCR/src/main/resources/static/js/invoice.js
+++ WebOCR/src/main/resources/static/js/invoice.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
 	$(function() {
 		$('[data-toggle="tooltip"]').tooltip();
 	});
+
+	// On delete item button click.
+	$('.closeItem').on('click', function(event) {
+		var id = event.currentTarget.id.split('-')[1];
+
+		$('#row-' + id).remove();
+	});
+
+	// On add item button click.
+	$('#deleteItems').on('click', function(event) {
+		$('#itemContainer').empty();
+	});
 });
